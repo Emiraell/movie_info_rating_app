11a7679329832c816b33208710c66f9a4d72e7c8
emiraelDev <emmzex19@gmail.com>
2024-05-14 00:43:30 +0100

    add: authslice code formatting

--- src/store/features/Auth.ts
+++ src/store/features/Auth.ts
@@ -1,36 +1,45 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+// user interface
 interface User {
   name: string;
   guestId: string | null;
-  expire: string | null;
+  status: string;
 }
 
-let storedUserId = localStorage.getItem("guestId");
-let storedUserName = localStorage.getItem("name");
-
+// initial state for authorized user stored in local storage
 const initialState: User = {
-  name: storedUserName || "",
-  guestId: storedUserId || null,
-  expire: null,
+  name: localStorage.getItem("guestId") || "",
+  guestId: localStorage.getItem("name") || null,
+  status: "",
 };
 export const AuthUserSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    LoginUser: (state, action) => {
+    // Action to login user
+    LoginUser: (state, action: PayloadAction<string>) => {
       state.guestId && (state.name = action.payload);
       localStorage.setItem("name", action.payload);
     },
   },
   extraReducers(builder) {
-    builder.addCase(getAuth.fulfilled, (state, action) => {
-      state.guestId = action.payload.guest_session_id;
-      state.guestId && localStorage.setItem("guestId", state.guestId);
-    });
+    builder
+      .addCase(getAuth.fulfilled, (state, action) => {
+        state.guestId = action.payload.guest_session_id;
+        state.guestId && localStorage.setItem("guestId", state.guestId);
+        state.status = "succesful";
+      })
+      .addCase(getAuth.pending, (state) => {
+        state.status = "pending";
+      })
+      .addCase(getAuth.rejected, (state) => {
+        state.status = "error";
+      });
   },
 });
 
+// Login user as a guest action/function
 export const getAuth = createAsyncThunk("getAuth", async () => {
   const options = {
     method: "GET",
@@ -40,14 +49,17 @@ export const getAuth = createAsyncThunk("getAuth", async () => {
         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjc1YzEyYjhlYTI4ODFkODRhODA4ZDZiOTgwODA0ZSIsInN1YiI6IjY2MTk5YWZjOTBjZjUxMDE3Y2EyNmYwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A4OG4SnjnTSJY4f6Kiy1HMCN5qxlVn2pa6xJImqLXvc",
     },
   };
-
+  // make a request to login the user as a guest and generate a guest id on success
   const res = await fetch(
     "https://api.themoviedb.org/3/authentication/guest_session/new",
     options
   );
   const result = res.json();
+
+  // return result on sucess
   return result;
 });
 
+// export actions
 export const { LoginUser } = AuthUserSlice.actions;
 export default AuthUserSlice.reducer;
