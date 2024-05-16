1fba39b1c26b664cb67f3d7b2d739b67fa877f18
emiraelDev <emmzex19@gmail.com>
2024-05-16 15:27:51 +0100

    add: deploy file and .env in git ignore file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -64,6 +64,7 @@ export default function Hero({ homePage, pageName }: HeroProps) {
               onSubmit={(e) => {
                 e.preventDefault();
                 fetchSearched();
+                setMovieToSearch("");
               }}
             >
               <input
